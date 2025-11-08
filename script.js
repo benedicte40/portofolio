e2269c45bcd34e0a538ba82d53064c3163934da9
benedicte40 <benediction738@gmail.com>
2025-11-08 21:19:21 +0100

    Mon portofolio

---
+++ script.js
@@ -0,0 +1,8 @@
+// ===== Menu burger pour mobile =====
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+        
